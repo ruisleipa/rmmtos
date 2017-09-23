7e439116fbc9c7422035051b0a6701d4f70d150c
Mikko Paukkonen <mikko.paukkonen@gmail.com>
2017-09-23 11:51:46 +0300

    new bitmap malloc

--- kernel/malloc.c
+++ kernel/malloc.c
@@ -1,109 +1,100 @@
 #include "malloc.h"
 #include "string.h"
 #include "panic.h"
-
-struct head
-{
-	struct head* prev;
-	struct head* next;
-	unsigned int free:1;
-};
-
-struct head* first;
-
-/* this is the address of the byte after the pool */
-struct head* overblock;
+#include "task.h"
 
 static unsigned int used_bytes;
 static unsigned int total_bytes;
+unsigned int total_blocks;
+void* pool_start;
+
+#define SIZE_COUNT 6
+
+
+struct size_block {
+	unsigned int size;
+	unsigned int count;
+	unsigned int expand;
+};
+
+
+struct size_block zones[SIZE_COUNT] = {
+	{4, 0, 1},
+	{8, 0, 1},
+	{18, 0, 3},
+	{36, 0, 1},
+	{512, 8, 0},
+	{sizeof(struct Task), 2, 0}
+};
+
+#define BITMAP_SIZE 100
+
+//TODO: check if really zero allocated
+unsigned char bitmap[BITMAP_SIZE] = {0};
 
 void malloc_stats()
 {
-	unsigned int total_blocks = 0;
-	struct head* curr=first;
-
-	do
-	{
-		total_blocks++;
-
-		if(curr==overblock)
-			break;
-	}
-	while(curr=curr->next);
-
 	printf("Heap usage: %d/%d B in %d blocks\n", used_bytes, total_bytes, total_blocks);
-}
-
-void merge_free_blocks(struct head* block)
-{
-	struct head* first_free=block;
-	struct head* last_free=block;
-
-	while(first_free->prev && first_free->prev->free)
-		first_free=first_free->prev;
-
-	while(last_free != overblock && last_free->free)
-		last_free=last_free->next;
-
-	first_free->next=last_free;
-	last_free->prev=first_free;
 }
 
 void* malloc(unsigned int size)
 {
-	struct head* curr=first;
-	unsigned int curr_size;
+	int current_block = 0;
+	unsigned int address = (unsigned int)pool_start;
+	int i = 0;
+	int j = 0;
 
-	do
-	{
-		if(curr==overblock)
-			break;
+	while(zones[i].size < size) {
+		debug_printf("j%x\t", zones[i].count);
+		current_block += zones[i].count;
 
-		if(!curr->free)
-			continue;
+		i++;
+	}
 
-		curr_size=(unsigned int)curr->next-(unsigned int)curr;
-		curr_size-=sizeof(struct head);
 
-		/* OK, free block found */
 
-		if(curr_size<size)
-			continue;
+	while(bitmap[current_block / 8] == 0xff && current_block < total_blocks) {
+		// move to first block of next byte
+		current_block = (current_block + 8) & (~0x7);
+	}
 
-		/* OK, the block is large enough */
-		/* Now we find out if the block can be split */
+	debug_printf("b%a\t", bitmap[current_block / 8]);
+	debug_printf("m%a\t", (0x80 >> (current_block % 8)));
+	debug_printf("r%x\t", bitmap[current_block / 8] & (0x80 >> (current_block % 8)));
+	debug_printf("s%x\t", current_block);
 
-		if(curr_size>sizeof(struct head)+size)
-		{
-			/* do splitting */
+	while(current_block < total_blocks && (bitmap[current_block / 8] & (0x80 >> (current_block % 8))) != 0) {
+		debug_printf("s%x\t", current_block);
+		current_block++;
+	}
 
-			struct head* new_head=(char*)curr+sizeof(struct head)
-						+size;
+	debug_printf("u%x\t", current_block);
 
-			/* setup the new block header */
-			new_head->next=curr->next;
-			new_head->prev=curr;
-			new_head->free=1;
+	// block found, get an address for it
+	if(current_block < total_blocks) {
+		int j = 0;
 
-			/* setup the next block */
-			if(curr->next!=overblock)
-				curr->next->prev=new_head;
-
-			/* set the splitted block */
-			curr->next=new_head;
+		for(i = 0; i < SIZE_COUNT; ++i) {
+			if(j + zones[i].count <= current_block) {
+				j += zones[i].count;
+				address += zones[i].count * zones[i].size;
+			} else {
+				address += zones[i].size * (current_block - j);
+				break;
+			}
 		}
 
-		curr->free=0;
+		bitmap[current_block / 8] |= (0x80 >> (current_block % 8));
+		used_bytes += zones[i].size;
 
-		used_bytes += (unsigned int)curr->next-(unsigned int)curr;
+		debug_printf("\nalloc %x %x\n", address, current_block);
+		malloc_info_terse();
 
-		/*malloc_stats();*/
-
-		return (void*)((unsigned int)curr+sizeof(struct head));
+		return (void*)address;
 	}
-	while(curr=curr->next);
-malloc_info();
-printf("alloc: %d\n", size);
+
+	malloc_info();
+	printf("alloc: %d\n", size);
 	panic("out of mem");
 
 	return 0;
@@ -125,90 +116,102 @@ void* realloc(void* block, unsigned int new_size)
 
 void free(void* block)
 {
-	struct head* curr=first;
+	unsigned int address = (unsigned int)block - (unsigned int)pool_start;
+	unsigned int current_block = 0;
+	unsigned int zone = 0;
 
-	do
-	{
-		if(curr==overblock)
-			break;
-
-		if(curr==(char*)block-sizeof(struct head))
-		{
-			curr->free = 1;
-
-			used_bytes -= (unsigned int)curr->next-(unsigned int)curr;
-
-			merge_free_blocks(curr);
-
-			/*malloc_stats();*/
-
-			break;
-		}
+	while(address >= zones[zone].count * zones[zone].size && zone < SIZE_COUNT) {
+		address -= zones[zone].count * zones[zone].size;
+		current_block += zones[zone].count;
+		zone++;
 	}
-	while(curr=curr->next);
+
+	if(zone == SIZE_COUNT) {
+		panic("invalid free");
+	}
+
+	current_block += address / zones[zone].size;
+
+	// mark as free
+	bitmap[current_block / 8] ^= (0x80 >> (current_block % 8));
+	used_bytes -= zones[zone].size;
+
+	debug_printf("freed %x %x\n", block, current_block);
+	malloc_info_terse();
 }
+
 
 void malloc_init(void* start,void* end)
 {
-	unsigned int size;
+	unsigned int allocated_size = 0;
+	unsigned int pool_size;
+	unsigned int first_phase_increment = 0;
+	int i;
 
-	first=start;
-	overblock=(void*)(((char*)end)+1);
+	pool_start = start;
 
-	size=(unsigned int)overblock-(unsigned int)first-1;
+	// TODO: check this
+	pool_size=(unsigned int)end-(unsigned int)start	;
+
+	for (i = 0; i < SIZE_COUNT; ++i) {
+		allocated_size += zones[i].size * zones[i].count;
+		first_phase_increment += zones[i].size * zones[i].expand;
+	}
+
+	while (allocated_size + first_phase_increment <= pool_size) {
+		for (i = 0; i < SIZE_COUNT; ++i) {
+			zones[i].count += zones[i].expand;
+		}
+
+		allocated_size += first_phase_increment;
+	}
+
+	while (allocated_size + zones[0].size <= pool_size) {
+		zones[0].count++;
+		allocated_size += zones[0].size;
+	}
+
+	total_blocks = 0;
+
+	for (i = 0; i < SIZE_COUNT; ++i) {
+		total_blocks += zones[i].count;
+	}
+
+	if(total_blocks > BITMAP_SIZE * sizeof(unsigned char) * 8) {
+		panic("malloc bitmap");
+	}
+
+	memset(bitmap, 0, BITMAP_SIZE);
 
 	used_bytes = 0;
-	total_bytes = size;
+	total_bytes = allocated_size;
 
-	/*printf("heap: %x-%x, %dB\n",start,end,size);
-*/
-	memset(first,0,size);
-
-	first->prev=0;
-	first->next=overblock;
-	first->free=1;
+	malloc_info();
 }
 
 void malloc_info_terse()
 {
-	struct head* curr=first;
-	unsigned int curr_size;
+	int i;
 
-	do
-	{
-		if(curr==overblock)
-			break;
+	debug_printf("bitmap: ");
 
-		curr_size=(unsigned int)curr->next-(unsigned int)curr;
-		curr_size-=sizeof(struct head);
-
-		printf("%d %d\t", curr->free, curr_size);
+	for(i = 0; i < total_blocks / 8 + 1; ++i) {
+		debug_printf("%a  ", bitmap[i]);
 	}
-	while(curr=curr->next);
 
-	printf("\n");
+	debug_printf("\n");
 }
 
 void malloc_info()
 {
-	struct head* curr=first;
-	unsigned int curr_size;
+	int i;
 
-	printf("Malloc info:\n");
+	debug_printf("malloc zones:\n");
 
-	do
-	{
-		if(curr==overblock)
-			break;
-
-		curr_size=(unsigned int)curr->next-(unsigned int)curr;
-		curr_size-=sizeof(struct head);
-
-		if(!curr->free) {
-			printf("%x %d x %x B n: %x p: %x\t\t",curr,curr->free,curr_size,curr->next,curr->prev);
-		}
-
+	for(i = 0; i < SIZE_COUNT; ++i) {
+		debug_printf("%d blocks of %d bytes = %d bytes in total\n", zones[i].count, zones[i].size, zones[i].size * zones[i].count);
 	}
-	while(curr=curr->next);
+
+	malloc_stats();
 }
 
