2e3fc6b63a87cc194a711c657889fc7195cca207
Mikko Paukkonen <mikko.paukkonen@gmail.com>
2017-09-26 19:03:08 +0300

    fat things

--- kernel/fs/fat/fat.c
+++ kernel/fs/fat/fat.c
@@ -2,6 +2,8 @@
 #include "fs/file.h"
 #include "fs/directory.h"
 
+unsigned int fat_read_file(struct FileHandle* handle, char* buffer, unsigned int count);
+
 struct Parameters
 {
 	unsigned int bytes_per_sector;
@@ -37,6 +39,9 @@ struct FatRootDirectory
 
 	Uint64 root_begin;
 	Uint64 root_end;
+	Uint64 fat_begin;
+	Uint64 data_begin;
+	unsigned char cluster_pot;
 	struct FileHandle* handle;
 };
 
@@ -45,7 +50,14 @@ struct FatFile
 	struct File super;
 
 	char name[8+3+1];
+	struct FatRootDirectory* root;
 	Uint64 entry_position;
+};
+
+struct FileOps fat_file_ops = {
+	0, /* open */
+	fat_read_file, /* read */
+	0, /* write */
 };
 
 struct Parameters* fat_read_parameters(struct Parameters* params, struct FileHandle* handle)
@@ -53,7 +65,7 @@ struct Parameters* fat_read_parameters(struct Parameters* params, struct FileHan
 	char* mbr = malloc(512);
 	Uint64 zero;
 
-	init64_32(&zero, 0, 0);
+	init64(&zero, 0, 0, 0, 0);
 
 	if(mbr)
 	{
@@ -69,19 +81,37 @@ struct Parameters* fat_read_parameters(struct Parameters* params, struct FileHan
 		memcpy(&params->hidden_sectors, &mbr[28], sizeof(params->hidden_sectors));
 		memcpy(&params->total_sectors, &mbr[32], sizeof(params->total_sectors));
 
+		printf("fat spc: %x bps: %x\n", params->sectors_per_cluster, params->bytes_per_sector);
+		printf("fat rootent: %x secs: %x\n", params->root_entries, (params->root_entries *32 + params->bytes_per_sector - 1) / params->bytes_per_sector);
+
 		free(mbr);
 	}
 }
 
 void parse_params(struct Parameters* params, struct FatRootDirectory* directory)
 {
+	unsigned int bytes_per_cluster;
+
+	directory->cluster_pot = 0;
+
+	bytes_per_cluster = params->sectors_per_cluster * params->bytes_per_sector;
+
+	while(!(bytes_per_cluster & 1)) {
+		directory->cluster_pot++;
+		bytes_per_cluster >>= 1;
+	}
+
+	debug_printf("directory->cluster_pot: %x\n", directory->cluster_pot);
+
 	init64(&directory->root_begin, 0, 0, 0, (params->reserved_sectors + (params->fats * params->sectors_per_fat)));
-
-	init64(&directory->root_end, 0, 0, 0, params->root_entries * 32);
-	add64(&directory->root_end, &directory->root_begin);
-
 	shl64(&directory->root_begin, 9);
-	shl64(&directory->root_end, 9);
+
+	set64(&directory->root_end, &directory->root_begin);
+	add64_16(&directory->root_end, ((params->root_entries * 32 + params->bytes_per_sector - 1) / params->bytes_per_sector) * params->bytes_per_sector);
+	// TODO: Round this up to the next sector
+
+	init64(&directory->fat_begin, 0, 0, 0, params->reserved_sectors);
+	shl64(&directory->fat_begin, 9);
 }
 
 #define FLAG_DIRECTORY 0x10
@@ -119,11 +149,150 @@ struct Node* fat_root_get_next_node(struct DirectoryHandle* handle, struct FatFi
 		}
 
 		{
-			struct File* file = file_create_node(0, 0, 0);
+			struct File* file = file_create_node(0, 0, &fat_file_ops);
 			struct FatFile* fat_file = realloc(file, sizeof(*fat_file));
 
 			memcpy(fat_file->name, &entry.name, 8 + 3);
 			fat_file->name[8 + 3] = 0;
+			fat_file->root = directory;
+
+			// XXX: self referential pointer, invalid on realloc
+			fat_file->super.super.name = fat_file->name;
+
+			set64(&fat_file->entry_position, &position);
+
+			return fat_file;
+		}
+	}
+
+	return 0;
+}
+
+void name_to_fat(char* name) {
+
+}
+
+void name_from_fat(char* name) {
+
+}
+
+unsigned int fat_read_file(struct FileHandle* handle, char* buffer, unsigned int count) {
+	struct FatFile* file = handle->super.node;
+	struct DirectoryEntry entry;
+	unsigned int current_cluster;
+	unsigned int cluster_offset;
+	Uint64 whole_clusters_left;
+	Uint64 position;
+	unsigned int completed = 0;
+
+	set64(&whole_clusters_left, &handle->position);
+	cluster_offset = shr64(&whole_clusters_left, file->root->cluster_pot);
+
+	file_seek(file->root->handle, &file->entry_position);
+	file_read(file->root->handle, &entry, sizeof(entry));
+
+	current_cluster = entry.cluster_lo;
+
+	while(completed < count) {
+		unsigned int next_cluster;
+
+		printf("cluster_offset: %d current_cluster: %d", cluster_offset, current_cluster);
+
+		// TODO: handle bad clusters?
+		if(cmp64_16(&whole_clusters_left, 0) > 0)
+		{
+			// just skipping
+			dec64(&whole_clusters_left);
+		} else {
+			// cluster is at least partially to be read to buffer
+			unsigned int size = (1 << file->root->cluster_pot) - cluster_offset;
+
+			if(size > (count - completed)) {
+				size = (count - completed);
+			}
+
+			init64(&position, 0, 0, 0, current_cluster - 2);
+			shl64(&position, file->root->cluster_pot);
+			add64(&position, &file->root->root_end);
+			file_seek(file->root->handle, &position);
+
+			printf("reading %d bytes from cluster @ %x%x\n", size, position.i[1], position.i[0]);
+
+			size = file_read(file->root->handle, buffer, size);
+
+			printf("read %x bytes from cluster\n", size);
+
+			buffer += size & FILE_IO_RESULT_SIZE_MASK;
+			completed += size & FILE_IO_RESULT_SIZE_MASK;
+
+			printf("completed %x count %x \n", completed, count);
+
+			// After reading the first partial cluster the read will always begin at the start of a cluster
+			cluster_offset = 0;
+
+			// TODO: handle file end
+		}
+
+		init64(&position, 0, 0, 0, current_cluster + (current_cluster / 2));
+		add64(&position, &file->root->fat_begin);
+
+		file_seek(file->root->handle, &position);
+		file_read(file->root->handle, &next_cluster, sizeof(next_cluster));
+
+		if(current_cluster % 2) {
+			next_cluster >>= 4;
+		} else {
+			next_cluster &= 0x0FFF;
+		}
+
+		if (next_cluster >= 0xFF8) {
+			return completed;
+		}
+
+		current_cluster = next_cluster;
+	}
+
+	return completed;
+}
+
+struct Node* fat_root_get_node_by_name(struct DirectoryHandle* handle, char* name)
+{
+	struct FatRootDirectory* directory = handle->super.node;
+	Uint64 position;
+	struct DirectoryEntry entry;
+
+	set64(&position, &directory->root_begin);
+
+	file_seek(directory->handle, &position);
+
+	while(cmp64(&position, &directory->root_end) < 0) {
+		set64(&position, &directory->handle->position);
+		file_read(directory->handle, &entry, sizeof(entry));
+
+		if(entry.attributes & FLAG_VOLUME) {
+			continue;
+		}
+
+		if(entry.name[0] == 0xe5) {
+			continue;
+		}
+
+		if(entry.name[0] == 0x00) {
+			break;
+		}
+
+		if(memcmp(entry.name, name) != 0) {
+			continue;
+		}
+
+		{
+			struct File* file = file_create_node(0, 0, &fat_file_ops);
+			struct FatFile* fat_file = realloc(file, sizeof(*fat_file));
+
+			memcpy(fat_file->name, &entry.name, 8 + 3);
+			fat_file->name[8 + 3] = 0;
+
+			fat_file->root = directory;
 
 			// XXX: self referential pointer, invalid on realloc
 			fat_file->super.super.name = fat_file->name;
@@ -140,7 +309,7 @@ struct Node* fat_root_get_next_node(struct DirectoryHandle* handle, struct FatFi
 static struct DirectoryOps fat_root_dir_ops = {
 	0,
 	fat_root_get_next_node,
-	0,
+	fat_root_get_node_by_name,
 	0
 };
 
--- kernel/fs/file.h
+++ kernel/fs/file.h
@@ -15,8 +15,16 @@ struct File* file_create_node(char* name, unsigned int flags, struct FileOps* op
 
 struct FileHandle* file_open(struct Node* file, unsigned int mode);
 void file_close(struct FileHandle* handle);
+
+#define FILE_IO_RESULT_STATUS_MASK 0xf000
+#define FILE_IO_RESULT_SIZE_MASK 0x0fff
+
+#define FILE_IO_STATUS_SUCCESS 0
+#define FILE_IO_STATUS_ERROR 0x1000
+
 unsigned int file_read(struct FileHandle* handle, char* buffer, unsigned int size);
 unsigned int file_write(struct FileHandle* handle, char* buffer, unsigned int size);
+
 void file_seek(struct FileHandle* handle, Uint64* position);
 
 typedef struct FileHandle* (FileOpen)(struct File* file, unsigned int mode);
